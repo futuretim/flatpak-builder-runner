6ee4737d14c5b3ecf68bde875dc59160a34becd9
Tim Howard <timothy.howard@gmail.com>
2017-04-17 12:35:09 -0400

    initial add

---
+++ spec/flatpak-builder-runner-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FlatpakBuilderRunnerView from '../lib/flatpak-builder-runner-view';
+
+describe('FlatpakBuilderRunnerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
